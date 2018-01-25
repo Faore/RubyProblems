9e5dc3d8f08f4f271efecc8495f0646e01b49947
Chris Thomas <faore@onhaven.net>
2018-01-25 04:35:33 -0800

    Finish the MyQueue. Still needs documentation.

--- MyQueueProblem/my_queue.rb
+++ MyQueueProblem/my_queue.rb
@@ -1,16 +1,35 @@
+require_relative './stack'
+
 module MyQueueProblem
   class MyQueue
+    @storage_stack
+    @helper_stack
+
+    def initialize
+      @storage_stack = Stack.new
+      @helper_stack = Stack.new
+    end
 
     def push(x)
-      throw NotImplementedError
+      # We need to get the new value to the bottom of the storage stack.
+      # First move the entire storage stack into the helper stack.
+      until @storage_stack.empty?
+        @helper_stack.push @storage_stack.pop
+      end
+      # Now that the storage stack is empty. Put this at the bottom.
+      @storage_stack.push x
+      # Put everything in the helper stack back on top of the new item.
+      until @helper_stack.empty?
+        @storage_stack.push @helper_stack.pop
+      end
     end
 
-    def pop(x)
-      throw NotImplementedError
+    def pop
+      @storage_stack.pop
     end
 
-    def peek()
-      throw NotImplementedError
+    def peek
+      @storage_stack.peek
     end
   end
 end
--- MyQueueProblem/test_my_queue.rb
+++ MyQueueProblem/test_my_queue.rb
@@ -1,4 +1,25 @@
+
+require_relative './my_queue'
+require 'test/unit'
+
 module MyQueueProblem
-  class TestMyQueue
+  class TestMyQueue < Test::Unit::TestCase
+    # Push 5 items to the queue, peek at the first one,
+    # and pop the 5 items from the queue and ensure they are in order.
+    def test_stack
+      input = [1,2,3,4,5]
+      queue = MyQueue.new
+
+      input.each do |t|
+        queue.push t
+      end
+      assert_equal(1, queue.peek)
+
+      output = Array.new
+      5.times do |i|
+        output.push(queue.pop)
+      end
+      assert_equal(input, output)
+    end
   end
 end
